5857273a740f8de3643432f4708f1cc958edc3bb
Pratham Garg <aggarwal.prathamgarg@gmail.com>
2022-09-22 00:02:31 +0530

    Created timer and clock animation

---
+++ Clock Animation/script.js
@@ -0,0 +1,40 @@
+setInterval(setClock, 1000);
+
+function setClock() {
+    const hCircle = document.querySelector('.circle:nth-of-type(3) svg circle');
+    const mCircle = document.querySelector('.circle:nth-of-type(2) svg circle');
+    const sCircle = document.querySelector('.circle:nth-of-type(1) svg circle');
+    const hour = document.querySelector('.hour ');
+    const minute = document.querySelector('.minute ');
+    const second = document.querySelector('.second ');
+    const ampm = document.querySelector('.ampm ');
+    const hdot = document.querySelector('.circle:nth-of-type(3) .dot');
+    const mdot = document.querySelector('.circle:nth-of-type(2) .dot');
+    const sdot = document.querySelector('.circle:nth-of-type(1) .dot');
+
+
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+    s = s < 10 ? '0' + s : s;
+    minute.innerHTML = m + ":";
+    second.innerHTML = s;
+    ampm.innerHTML = h >= 12 ? 'PM' : 'AM';
+
+    h = h >= 12 ? h - 12 : h;
+    hour.innerHTML = h + ":";
+
+    hdot.style.transform = `rotate(${h * 30}deg)`;
+    mdot.style.transform = `rotate(${m * 6}deg)`;
+    sdot.style.transform = `rotate(${s * 6}deg)`;
+
+    hCircle.style.strokeDashoffset = 942 - (h * 942) / 12;
+    mCircle.style.strokeDashoffset = 1257 - (m * 1257) / 60;
+    sCircle.style.strokeDashoffset = 1571 - (s * 1571) / 60;
+}
+
+setClock();
---
+++ Timer Mobile/script.js
@@ -0,0 +1,62 @@
+const second = document.querySelector('.second');
+const minute = document.querySelector('.minute');
+const hour = document.querySelector('.hour');
+const timer = document.querySelector('.timer');
+const times = document.querySelectorAll('.time');
+let playing = false;
+let interval;
+
+function run() {
+    if (playing) {
+        playing = false;
+        clearInterval(interval);
+        return;
+    }
+    playing = true;
+    interval = setInterval(() => {
+        let s = parseInt(second.innerHTML);
+        let m = parseInt(minute.innerHTML);
+        let h = parseInt(hour.innerHTML);
+
+        s = s + 1;
+        if (s == 60) {
+            let audio = new Audio(
+                'https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
+            audio.play();
+            s = 00;
+            m = m + 1;
+            if (m == 60) {
+                m = 00;
+                h = h + 1;
+                hour.innerText = h + ":";
+                if (h < 10) {
+                    hour.innerText = '0' + h + ":";
+                }
+            }
+            minute.innerText = m + ': ';
+            if (m < 10) {
+                minute.innerText = '0' + m + ":";
+            }
+
+
+        }
+        second.innerText = s;
+        if (s < 10) {
+            second.innerText = '0' + s;
+        }
+
+        if (h == 0) {
+            hour.style.display = 'none';
+        } else {
+            hour.style.display = 'inline-block';
+            times.forEach((time) => {
+                time.classList.add('small');
+            });
+        }
+
+    }, 1000);
+}
+
+timer.onclick = () => {
+    run();
+}
